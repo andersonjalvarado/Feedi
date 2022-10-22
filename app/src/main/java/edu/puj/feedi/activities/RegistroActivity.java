f9f009e883031266ee1ca5688cdcdbcd2330e1f8
andersonjalvarado <anderson__alvarado@hotmail.com>
2022-10-22 10:58:55 -0500

    Icon

--- app/src/main/java/edu/puj/feedi/activities/RegistroActivity.java
+++ app/src/main/java/edu/puj/feedi/activities/RegistroActivity.java
@@ -3,7 +3,10 @@ package edu.puj.feedi.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.puj.feedi.databinding.ActivityRegistroBinding;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import edu.puj.feedi.R;
 
@@ -16,5 +19,13 @@ public class RegistroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityRegistroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.createButton.setOnClickListener(view -> {
+            String nombre = binding.createDisplayName.getEditText().getText().toString();
+            Toast.makeText(getBaseContext(), String.format("%s registrado con éxito, inicia sesión", nombre),Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this,LoginActivity.class);
+           // intent.putExtra("nombre",nombre);
+            startActivity(intent);
+        });
     }
 }

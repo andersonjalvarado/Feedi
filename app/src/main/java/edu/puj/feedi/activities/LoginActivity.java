054c62148abb9bcb57a6e56a9a7fc96f39f95672
andersonjalvarado <anderson__alvarado@hotmail.com>
2022-10-22 19:07:54 -0500

    Firebase Sign up

--- app/src/main/java/edu/puj/feedi/activities/LoginActivity.java
+++ app/src/main/java/edu/puj/feedi/activities/LoginActivity.java
@@ -6,18 +6,21 @@ import edu.puj.feedi.databinding.ActivityLoginBinding;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import edu.puj.feedi.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        this.setTitle("Inicio de Sesión");
         binding.signupButton.setOnClickListener(view -> startActivity(new Intent(this, RegistroActivity.class)));
     }
 }
--- app/src/main/java/edu/puj/feedi/activities/RegistroActivity.java
+++ app/src/main/java/edu/puj/feedi/activities/RegistroActivity.java
@@ -1,18 +1,29 @@
 package edu.puj.feedi.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.puj.feedi.databinding.ActivityRegistroBinding;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.puj.feedi.R;
 
 public class RegistroActivity extends AppCompatActivity {
-
+    public static final String TAG = RegistroActivity.class.getName();
     ActivityRegistroBinding binding;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +31,68 @@ public class RegistroActivity extends AppCompatActivity {
         binding = ActivityRegistroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.createButton.setOnClickListener(view -> {
-            String nombre = binding.createDisplayName.getEditText().getText().toString();
-            Toast.makeText(getBaseContext(), String.format("%s registrado con éxito, inicia sesión", nombre),Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(this,LoginActivity.class);
-           // intent.putExtra("nombre",nombre);
-            startActivity(intent);
+        firestore = FirebaseFirestore.getInstance();
+
+        this.setTitle("Crear Nuevo Perfil");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        binding.createButton.setOnClickListener(view -> doSignUp());
+    }
+
+    private void doSignUp() {
+        String email = binding.createEmail.getEditText().getText().toString();
+        String pwd = binding.createPass.getEditText().getText().toString();
+        String nombre = binding.createDisplayName.getEditText().getText().toString();
+        String cel = binding.createPhone.getEditText().getText().toString();
+        String rol = " ";
+
+        if (binding.radioClient.isChecked())
+        {
+            rol = "Cliente";
+        }
+        if (binding.radioRestaurant.isChecked())
+        {
+            rol = "Restaurante";
+        }
+        else{
+            Toast.makeText(getBaseContext(), "Seleccione un rol", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(email == null || email.isEmpty() || pwd == null || pwd.isEmpty() || nombre == null || nombre.isEmpty()|| cel == null || cel.isEmpty()){
+            Toast.makeText(getBaseContext(), "Los campos no pueden estar vacios", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else
+            postUser(email, pwd, nombre, cel, rol);
+
+    }
+
+    private void postUser(String email, String pwd, String nombre, String cel, String rol){
+        Map<String, Object> map = new HashMap<>();
+        map.put("email", email);
+        map.put("password", pwd);
+        map.put("nombre", nombre);
+        map.put("celular", cel);
+        map.put("rol", rol);
+
+        firestore.collection("Usuario").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getBaseContext(), String.format("%s registrado con éxito, inicia sesión", nombre),Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getBaseContext(), "Error en los registros", Toast.LENGTH_LONG).show();
+            }
         });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return false;
     }
 }
